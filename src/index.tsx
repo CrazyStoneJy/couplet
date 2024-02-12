d036ae8e209235e6618b04a7942164ab6adfde13
CrazyStoneJy <crazystone13753251592@gmail.com>
2024-02-12 21:45:45 +0800

    chore

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,26 @@
+import pinyin, { Pinyin } from "pinyin";
+import { ENUM_PINYIN_STYLE } from "pinyin/lib/constant";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 function HomePage() {
+
+    const [text, setText] = useState('');
+    
+    useEffect(() => {
+        const r: string[][] = pinyin('出光灿烂', {
+            style: ENUM_PINYIN_STYLE.TONE2
+        });
+        // setText(r);
+        console.log(r);
+        
+    }, []);
+
+    
+
     return (
         <View>
-            <Text>Home</Text>
+            <Text style={{ margin: 20, fontSize: 16 }}>{text}</Text>
         </View>
     );
 }
